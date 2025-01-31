89945443e6f5856dce1c271e9a2e9299ddd91110
WallaceSantana79 <wallace.sanntana@souunit.com.br>
2025-01-31 12:19:34 -0300

    Comentário

---
+++ Aulas/Exercícios-Testes-Aulas/assets/js/aula4/comentarios.js
@@ -0,0 +1,9 @@
+/* Comentário
+longo*/
+console.log('Olá mundo!') // Outro comentário
+console.log('Linha 1')
+// Comentário
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+// Vai ser ignorado
