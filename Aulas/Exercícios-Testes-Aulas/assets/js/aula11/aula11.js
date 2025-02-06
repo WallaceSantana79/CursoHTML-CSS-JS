3698b28e10c7ff252d64f6c3225b36c81f927e27
WallaceSantana79 <wallace.sanntana@souunit.com.br>
2025-02-06 10:53:57 -0300

    Alert,confirm e prompt

---
+++ Aulas/Exercícios-Testes-Aulas/assets/js/aula11/aula11.js
@@ -0,0 +1,3 @@
+window.alert('Com a nossa mensagem.');
+window.confirm('Deseja realmente apagar?')
+window.prompt('Digite o seu nome: ')
---
+++ Aulas/Exercícios-Testes-Aulas/assets/js/aula11/exercicio.js
@@ -0,0 +1,6 @@
+const num1 = Number(prompt("Digite um número:"))
+const num2 = Number(prompt("Digite um segundo número:"))
+
+
+
+const resultado = alert(`O resultado foi ${num1 + num2}`)
