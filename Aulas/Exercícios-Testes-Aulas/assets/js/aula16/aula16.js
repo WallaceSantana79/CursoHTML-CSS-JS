138e35b8ceed7450f459248f38ca9913f079ab8c
WallaceSantana79 <wallace.sanntana@souunit.com.br>
2025-02-18 12:42:36 -0300

    Arrays (básico)

---
+++ Aulas/Exercícios-Testes-Aulas/assets/js/aula16/aula16.js
@@ -0,0 +1,21 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos[0] = 'Eduardo';
+// alunos[alunos.lenght] = 'Luiza'; // Adiciona no fim
+// alunos[alunos.lenght] = 'Fábio'; // Adiciona no fim
+// alunos[alunos.lenght] = 'Luana'; // Adiciona no fim
+// alunos.push('Otávio'); // Adiciona no fim
+// alunos.push('Luiza'); // Adiciona no fim
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio'); // Adiciona no começo
+
+// alunos.pop(); // Retiro o último elemento do array
+// alunos.shift(); // Retira o primeiro elemento do array
+// const removido = alunos.pop() // É possível guardar o elemento removido
+// console.log(removido); 
+// delete alunos[1]; // Deleta um elemento específico
+// console.log(alunos);
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
