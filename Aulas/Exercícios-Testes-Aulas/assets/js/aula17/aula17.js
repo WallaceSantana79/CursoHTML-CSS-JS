f834aaf3e196974218da033e339633f1f24ddae8
WallaceSantana79 <wallace.sanntana@souunit.com.br>
2025-02-19 21:04:19 -0300

    Funções

---
+++ Aulas/Exercícios-Testes-Aulas/assets/js/aula17/aula17.js
@@ -0,0 +1,24 @@
+// function saudacao(nome){
+//     return `Bom dia, ${nome}!`;
+// }
+
+// const variavel = saudacao('Luiz');
+// console.log(variavel);
+
+// function soma(x = 1,y = 1){
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// const resultado = soma();
+// console.log(resultado);
+// const resultado1 = soma(4);
+// console.log(resultado1);
+// const resultado2 = soma(2,6);
+// console.log(resultado2);
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
