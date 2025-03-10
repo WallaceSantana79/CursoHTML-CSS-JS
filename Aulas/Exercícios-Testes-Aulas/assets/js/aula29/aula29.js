e21d3b687907a35129982c6adcb5fca482e345c9
Wallace Santana <wallace.santana@souunit.com.br>
2025-03-10 18:51:38 -0300

    Atribuição via desestruturação(arrays)

---
+++ Aulas/Exercícios-Testes-Aulas/assets/js/aula29/aula29.js
@@ -0,0 +1,21 @@
+// let a = 'A'; 
+// let b = 'B';
+// let c = 'C';
+
+// const letras = [b,c,a];
+// [a,b,c] = letras;
+
+// console.log(a,b,c);
+
+// const numeros = [1,2,3,4,5,6,7,8,9];
+// const [um, , três, , cinco, , sete] = numeros;
+// console.log(um, três, cinco);
+
+const numeros = [ [1,2,3],[4,5,6],[7,8,9] ];
+// const [,[, ,seis]] = numeros;
+// console.log(seis);
+
+const [lista1,lista2,lista3] = numeros;
+console.log(lista3[2]);
+
+
